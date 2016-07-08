fcee4047a93c2f0c568db92e16505a4dc660fe34
LodMacellos <marcellosevero27@gmail.com>
2016-07-07 22:47:25 -0700

    Add files via upload

---
+++ CommieQuest.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+
+public class CommieQuest {
+	public static void main(String[] args) {
+		// Creating new frame
+		JFrame frame = new JFrame("Commie Quest");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		
+		// Adding panel
+		Panel panel = new Panel();
+		frame.add(panel, BorderLayout.CENTER);
+		
+		Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int)screensize.getWidth();
+		int height = (int)screensize.getHeight();
+		frame.setSize(width, height);
+		frame.setVisible(true);
+	}
+}
---
+++ Panel.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class Panel extends JPanel {
+	// Class needs serialVersionUID
+	private static final long serialVersionUID = 1L;
+	
+	Panel(){	
+		setBackground(Color.white);
+	
+		setFocusable(true);
+	}	
+}
