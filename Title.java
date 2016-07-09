3a03f912d772ea75cb14abb596112416db0407d0
LodMacellos <marcellosevero27@gmail.com>
2016-07-09 16:04:14 -0700

    Update Title.java

--- Title.java
+++ Title.java
@@ -1,19 +1,32 @@
 package level;
 
 import java.awt.*;
-import javax.swing.*;
+import component.ImageResizeable;
 
 public class Title extends Level {
-	ImageIcon intro = new ImageIcon("resources/intro.png");
-	Image scaleIntro = intro.getImage().getScaledInstance((height - 50) * 4 / 3, height - 50, Image.SCALE_DEFAULT);
+	int heightIntro, xIntro;
+	ImageResizeable intro;
 	
 	public void resize(){
+		// Resize all images to fit screen size
+		super.resize();
+		heightIntro = height - 50;
+		xIntro = width / 2 - heightIntro * 2 / 3;
+		intro = new ImageResizeable("intro.png", xIntro, 0, -1, heightIntro);
 		
+		// Add or replace images in images array
+		if (images.size() > 0)
+			images.set(0, intro);
+		else {
+			images.add(intro);
+		}
 	}
 	
-	public void paint(Graphics g){
+	public void paintComponent(Graphics g){
+		// Background color
 		g.setColor(new Color(0, 47, 123));
 		g.fillRect(0, 0, width, height);
-		g.drawImage(scaleIntro, width / 2 - height * 2 /3, 0, null);
+		
+		super.paintComponent(g);
 	}
 }
