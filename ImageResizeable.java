c42ae969f5ab719275a34fe02eda24c97f3ad378
LodMacellos <marcellosevero27@gmail.com>
2016-07-09 16:07:51 -0700

    Update ImageResizeable.java

--- ImageResizeable.java
+++ ImageResizeable.java
@@ -4,27 +4,26 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 
 public class ImageResizeable {
-	public ImageIcon image;
-	Image scaledImage;
+	BufferedImage image = null;
 	public int x, y;
 	int width, height;
 	
 	public ImageResizeable(String imagePath, int x, int y, int width, int height){
-		BufferedImage image = null;
+		// Create unscaled image
 		try {
 			image = ImageIO.read(new File("resources/" + imagePath));
 		} catch (IOException e) {}
+		
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
-		scaledImage = image.getScaledInstance(width, height, Image.SCALE_FAST);
 	}
 	
 	public Image scaledImage(){
-		return scaledImage;
+		// Scale image based on given height and width
+		return image.getScaledInstance(width, height, Image.SCALE_FAST);
 	}
 }
