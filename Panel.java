ebffd245264dea3cd13dd0625e71a4c8ae0360a7
LodMacellos <marcellosevero27@gmail.com>
2016-07-08 17:15:13 -0700

    Update Panel.java

--- Panel.java
+++ Panel.java
@@ -1,15 +1,60 @@
 package main;
 
-import java.awt.Color;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Panel extends JPanel {
+public class Panel extends JPanel implements KeyListener, MouseListener, MouseMotionListener {
 	// Class needs serialVersionUID
 	private static final long serialVersionUID = 1L;
+	
+	// Update the frame
+	ActionListener update = new ActionListener(){
+		public void actionPerformed(ActionEvent e) {
+			repaint();
+		}
+	};
 	
 	Panel(){	
 		setBackground(Color.white);
 	
+		// Set update every 20 milliseconds
+		Timer timer = new Timer(20, update);
+		timer.start();
+		
+		// Creating event listeners
+		addKeyListener(this);
+		addMouseListener(this);
+		addMouseMotionListener(this);
 		setFocusable(true);
-	}	
+	}
+	
+	public void keyPressed(KeyEvent e){
+		int keyCode = e.getKeyCode();
+	}
+	
+	public void keyReleased(KeyEvent e){
+		int keyCode = e.getKeyCode();
+	}
+	
+	public void mousePressed(MouseEvent e){
+		int x = e.getX(), y = e.getY();
+	}
+	
+	public void mouseMoved(MouseEvent e){
+		int x = e.getX(), y = e.getY();
+	}
+	
+	// Unused events
+	public void keyTyped(KeyEvent e){}
+	public void mouseClicked(MouseEvent e){}
+	public void mouseReleased(MouseEvent e){}
+	public void mouseEntered(MouseEvent e){}
+	public void mouseExited(MouseEvent e){}
+	public void mouseDragged(MouseEvent e){}
+	
+	// Paint the frame
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+	}
 }
