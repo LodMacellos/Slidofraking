7c7c64a29d5b48551f2c7738511a4a3b6e89c3a0
LodMacellos <marcellosevero27@gmail.com>
2016-07-10 00:24:12 -0700

    Update Panel.java

--- Panel.java
+++ Panel.java
@@ -3,65 +3,66 @@ package main;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javafx.embed.swing.JFXPanel;
 import level.*;
 
-public class Panel extends JPanel implements KeyListener, MouseListener, MouseMotionListener {
+public class Panel extends JFXPanel implements KeyListener, MouseListener, MouseMotionListener {
 	// Class needs serialVersionUID
 	private static final long serialVersionUID = 1L;
-	
+
 	// Current level, start at 0 (title)
 	public static int level = 0;
-	
+
 	// Window size
 	static Dimension windowsize = CommieQuest.frame.getBounds().getSize();
 	public static int width = (int)windowsize.getWidth();
 	public static int height = (int)windowsize.getHeight();
-	
+
 	// Create all levels
 	Level title = new Title();
 	Level[] levels = new Level[] {title};
-	
+
 	// Update the frame
 	ActionListener update = new ActionListener(){
 		public void actionPerformed(ActionEvent e) {
 			windowsize = CommieQuest.frame.getBounds().getSize();
 			width = (int)windowsize.getWidth();
 			height = (int)windowsize.getHeight();
-			levels[level].resize();
+			levels[level].update();
 			repaint();
 		}
 	};
-	
+
 	Panel(){
 		setBackground(Color.white);
-	
+
 		// Set update every 20 milliseconds
 		Timer timer = new Timer(20, update);
 		timer.start();
-		
+
 		// Creating event listeners
 		addKeyListener(this);
 		addMouseListener(this);
 		addMouseMotionListener(this);
 		setFocusable(true);
 	}
-	
+
 	public void keyPressed(KeyEvent e){
 		int keyCode = e.getKeyCode();
 	}
-	
+
 	public void keyReleased(KeyEvent e){
 		int keyCode = e.getKeyCode();
 	}
-	
+
 	public void mousePressed(MouseEvent e){
 		int x = e.getX(), y = e.getY();
 	}
-	
+
 	public void mouseMoved(MouseEvent e){
 		int x = e.getX(), y = e.getY();
 	}
-	
+
 	// Unused events
 	public void keyTyped(KeyEvent e){}
 	public void mouseClicked(MouseEvent e){}
@@ -69,7 +70,7 @@ public class Panel extends JPanel implements KeyListener, MouseListener, MouseMo
 	public void mouseEntered(MouseEvent e){}
 	public void mouseExited(MouseEvent e){}
 	public void mouseDragged(MouseEvent e){}
-	
+
 	// Paint the frame
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
