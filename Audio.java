285a4a3d38e04cd69c0f2ea15afacf0c2b3314b5
LodMacellos <marcellosevero27@gmail.com>
2016-07-10 00:31:18 -0700

    Add files via upload

---
+++ Audio.java
@@ -0,0 +1,33 @@
+package media;
+
+import java.io.File;
+import javafx.scene.media.*;
+import javafx.util.Duration;
+
+public class Audio {
+	Media audio;
+	MediaPlayer playAudio;
+
+	public Audio(String filePath){
+		audio = new Media(new File("resources/" + filePath).toURI().toString());
+		playAudio = new MediaPlayer(audio);
+	}
+
+	public void play(){
+		playAudio.play();
+	}
+
+	public void stop(){
+		playAudio.stop();
+	}
+
+	public void loop(){
+		playAudio.play();
+		playAudio.setOnEndOfMedia(new Runnable() {
+			public void run() {
+				playAudio.seek(Duration.ZERO);
+				playAudio.play();
+		       }
+		   });
+	}
+}
